6f213d3657e4addd95d498be9aae296a81c375a1
jaggu <patiljagdish196139@gmail.com>
2024-12-18 20:26:42 +0530

    This is my First project from simplilearn

---
+++ src/main/java/mypkg1/MyCalc.java
@@ -0,0 +1,13 @@
+package mypkg1;
+public class MyCalc {	
+	public void show()
+	{
+		System.out.println("Hello DevOps, Welcome to Maven");
+	}
+	public static void main(String[] args) {
+
+		MyCalc ob = new MyCalc();
+		ob.show();
+	}
+
+}
