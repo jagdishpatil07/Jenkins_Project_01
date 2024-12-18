715f6385d3219d94741aefe89c52562bedbf18e3
Jagdish Patil <130397851+jagdishpatil07@users.noreply.github.com>
2024-12-18 20:30:56 +0530

    Update MyCalc.java

--- src/main/java/mypkg1/MyCalc.java
+++ src/main/java/mypkg1/MyCalc.java
@@ -2,7 +2,7 @@ package mypkg1;
 public class MyCalc {	
 	public void show()
 	{
-		System.out.println("Hello DevOps, Welcome to Maven");
+		System.out.println("Hello Jagdish, Welcome to Devops wold");
 	}
 	public static void main(String[] args) {
 
